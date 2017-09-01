d43a1fb929366c7ffff52c62f0f8daf83320e780
Junxxx <hprjunxxx@gmail.com>
2017-09-01 12:47:44 +0800

    接口签名加密算法

--- addons/awt_enroll/core/model/awtennis.php
+++ addons/awt_enroll/core/model/awtennis.php
@@ -11,6 +11,7 @@ if (!defined('IN_IA')) {
 class AWT_Tenroll_Awtennis {
 
     private static $apiUrl = '';
+    private static $key = 'testkey12123';
     private $salt = '';
 
     private $user = array();
@@ -18,6 +19,26 @@ class AWT_Tenroll_Awtennis {
     public function __construct()
     {
 
+    }
+
+    public function encrypt()
+    {
+        /*
+         * 1、将请求参数 按照键名对关联数组进行升序排序
+         * 2、把参数按键值对组成字符串key=value
+         * 3、在字符串末尾加上私钥key
+         * 4、将最终字符串进行MD5加密，字符串转为大写，得到签名
+        */
+        ksort($package);
+        $string1 = '';
+        foreach($package as $key => $v) {
+            if (empty($v)) {
+                continue;
+            }
+            $string1 .= "{$key}={$v}&";
+        }
+        $string1 .= "key={$wechat['key']}";
+        $sign = strtoupper(md5($string1));
     }
 
     /*获取积分排行榜*/
