24425cd96cb383e9af857cadf7e01f8987462633
junxxx <hprjunxxx@gmail.com>
2017-10-25 17:58:29 +0800

    积分榜

--- addons/awt_enroll/core/mobile/ranking/ranking.php
+++ addons/awt_enroll/core/mobile/ranking/ranking.php
@@ -6,6 +6,52 @@ if (!defined('IN_IA')) {
 global $_W, $_GPC;
 
 if ($_W['isajax']) {
-	show_json(0,'敬请期待!!!');
+    $url = 'http://info.sports.sina.com.cn/rank/atp.php?sina-fr=bd.ala.top.tennis';
+    $html = file_get_contents($url);
+    /*去掉http头*/
+    preg_match("/Content-Length:.?(\d+)/", $html, $matches);
+    @$length = $matches[1];
+    $html = substr($html, - $length);
+
+    /*gb2312编码转换为utf-8*/
+    $html = mb_convert_encoding($html, 'UTF-8', 'gb2312');
+
+    if (!stripos('"Content-Type"', $html) && !stripos('content="text/html;', $html))
+    {
+        $html = str_replace('charset=gb2312', 'charset=utf-8', $html);
+    }
+    $doc = new DOMDocument();
+    @$doc->loadHTML('<?xml encoding="UTF-8">'.$html);
+    $xpath = new DOMXPath($doc);
+    $playerList = $xpath->query('//table[@class="ranktable"]');
+    $ranking = array();
+    foreach ($playerList as $container)
+    {
+        $arr = $container->getElementsByTagName('tr');
+        $skip = 0;
+        foreach ($arr as $item)
+        {
+            $skip++;
+            if ($skip <= 2)
+            {
+                continue;
+            }
+            $row = $item->getElementsByTagName('td');
+            $player = array();
+            foreach ($row as $k => $v)
+            {
+                $player[] = $v->nodeValue;
+            }
+            $ranking[] = array(
+                'rank' => $player[0],
+                'name' => $player[2],
+                'country' => $player[3],
+                'score' => $player[4],
+                'attendMatch' => $player[5],
+            );
+        }
+    }
+
+    show_json(1, array('list' => $ranking));
 }
 include $this->template('main/ranking');
