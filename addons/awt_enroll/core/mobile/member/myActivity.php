08867204f2a120c83d068086d2008940a0d75085
junxxx <hprjunxxx@gmail.com>
2017-12-10 15:32:51 +0800

    myActivity html

--- addons/awt_enroll/core/mobile/member/myActivity.php
+++ addons/awt_enroll/core/mobile/member/myActivity.php
@@ -13,12 +13,17 @@ global $_W, $_GPC;
 $openid = m('user')->getOpenid();
 $member = m('member')->getMember($openid);
 
+$type = isset($_GPC['type']) ? $_GPC['type'] : '';
+// TODO 退赛的状态，还需另外处理
+
 if ($_W['isajax']) {
     $activityModel = m('activity');
-    $list = $activityModel->getActivitiesByMid($openid);
+    $list = $activityModel->getActivitiesByMid($openid, $type);
     if ($list) {
         foreach ($list as &$row) {
+            $row['headimg'] = tomedia($row['headimg']);
             $row['reserve_status'] = $activityModel::$enrollStatus[$row['reserve_status']];
+            $row['activity_stime'] = date('Y-m-d H:i', $row['activity_stime']);
         }
     }
 
